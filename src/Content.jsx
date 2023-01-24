18d3e20c49e0f48508ebb9b229e51a4284145e8a
JRisJunior <jeffmrubin@gmail.com>
2023-01-24 15:03:00 -0600

    update pull from backend rails app

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,25 @@
+import axios from 'axios';
+import {useState, useEffect} from "react";
+
 export function Content() {
+
+  const [environments, setEnvironments] = useState([]);
+
+  const handleIndexEnvironments = () => {
+    axios.get("http://localhost:3000/environments.json").then((response) => {
+      console.log(response.data);
+      setEnvironments(response.data);
+    });
+  };
+
+  useEffect(handleIndexEnvironments, []);
+
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <p>Hello hello</p>
     </div>
-  )
+  );
 }
+
+// https://data.cityofchicago.org/resource/a9u4-3dwb.json
